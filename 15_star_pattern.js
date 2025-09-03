7176ae85529900cc39277f9173dd49d354f65f1c
ganesh <bodakheganesh24@gmail.com>
2025-09-03 23:06:11 +0530

    date 2 sept 25  loops star pattern

---
+++ 15_star_pattern.js
@@ -0,0 +1,12 @@
+
+let n=5;
+for(let i=0;i<n;i++){
+    let str="";
+    for(let j=0;j<i;j++){
+        str+=" ";
+    }
+    for(let k=0;k<n-i; k++){
+        str+="*";
+    }
+    console.log(str);
+}
